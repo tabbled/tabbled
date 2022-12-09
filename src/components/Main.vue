59488c20a2821c4f0651db0b8b84a039f720d0dd
wulff007 <wulff007@gmail.com>
2022-12-09 09:43:38 +0300

    CodeEditor implemented

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,23 @@
 <template>
-
-    <div>ssswdwe</div>
+    sdsd
+    <CodeEditor>
+    </CodeEditor>
 
 </template>
 
 <script>
 import {defineComponent} from "vue";
+import CodeEditor from "./CodeEditor.vue"
+
+
 
 export default defineComponent({
+    components: {
+        CodeEditor
+    },
     data() {
         return {
-
+            query: "function ii() { return 1; } "
         }
     },
     methods: {
@@ -20,6 +27,15 @@ export default defineComponent({
 
 </script>
 
-<style scoped>
+<style lang="scss">
+
+.editor {
+    .el-card__body {
+        padding: 0;
+    }
+    .cm-content {
+        padding: 0;
+    }
+}
 
 </style>
