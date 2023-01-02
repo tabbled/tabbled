ac062a3819d6926ca2f1c7e67d42fe31a8aecb8e
wulff007 <wulff007@gmail.com>
2023-01-02 11:48:02 +0300

    DataSet action panel implemented

--- src/components/DataSetActionPanel.vue
+++ src/components/DataSetActionPanel.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-        <el-button v-if="canAdd" @click="add" size="small">Add</el-button>
-        <el-button v-if="canEdit" @click="edit" size="small">Edit</el-button>
-        <el-button v-if="canRemove && props.dataSet.selectedIds.length > 0" @click="remove" size="small">Remove</el-button>
+        <el-button v-if="canAdd" type="primary" @click="add" size="small">
+            {{t('add')}}
+        </el-button>
+        <el-button v-if="canEdit" @click="edit" size="small">
+            {{t('edit')}}
+        </el-button>
+        <el-button v-if="canRemove && props.dataSet.selectedIds.length > 0" @click="remove" size="small">
+            {{t('delete')}}
+        </el-button>
     </div>
 </template>
 
@@ -10,6 +16,9 @@
 import {DataSet} from "../model/dataset";
 import { ElMessageBox } from 'element-plus'
 import {ref, watch} from "vue"
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n();
 
 interface Props {
     dataSet: DataSet,
@@ -60,11 +69,11 @@ function remove() {
 
 
     ElMessageBox.confirm(
-        'Do you want to remove selected items?',
-        'Warning',
+        t('confirmDeleteTitle'),
+        t('delete'),
         {
-            confirmButtonText: 'Remove',
-            cancelButtonText: 'Cancel',
+            confirmButtonText: t('delete'),
+            cancelButtonText: t('cancel'),
             type: 'warning',
         }
     )
