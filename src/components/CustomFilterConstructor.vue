338d5aca763cb437b417c1c3afbb74096530c37a
wulff007 <wulff007@gmail.com>
2023-06-07 22:26:57 +0300

    custom filter implementation

--- src/components/CustomFilterConstructor.vue
+++ src/components/CustomFilterConstructor.vue
@@ -7,15 +7,17 @@
                :sortable="false"
     >
         <template #default="{item}">
-            <el-form size="small" label-position="top" label-width="80px" :show-message="false">
-
-
-
+            <el-form size="small" label-position="top" label-width="80px" :show-message="false" style="margin: 4px">
 
                 <div style="display: flex; flex-direction: row">
 
-                    <FieldSelect style="padding-right: 4px" :placeholder="$t('filters.field')" size="small" :data-source="dataSource" v-model="item['key']" />
-
+                    <FieldSelect style="padding-right: 4px"
+                                 :placeholder="$t('filters.field')"
+                                 size="small"
+                                 :data-source="dataSource"
+                                 v-model="item['key']"
+                                 @update:model-value="() => item['field'] = getField(item['key'])"
+                    />
 
                     <el-select :placeholder="$t('filters.operator')" size="small" v-model="item['op']">
                         <el-option
@@ -27,10 +29,73 @@
                     </el-select>
                 </div>
 
-                <div style="display: flex; flex-direction: row">
+                <div v-if="item['field'] && item['op'] !== 'any' && item['op'] !== 'empty'" style="display: flex; flex-direction: row; margin-top: 4px;">
+
+                    <div style="display: flex; flex-direction: row"  v-if="item['field'].type === 'number'">
+
+                        <el-input-number v-model="item['compare']"
+                                         :controls="false"
+                                         :precision="item['field'].precision ? item['field'].precision : 0"/>
+
+                        <div v-if="item['op'] === 'between'" style="margin-left: 4px; margin-right: 4px">-</div>
+                        <el-input-number v-if="item['op'] === 'between'"
+                                         v-model="item['compare_2']"
+                                         :controls="false"
+                                         :precision="item['field'].precision ? item['field'].precision : 0"/>
+                    </div>
+
+                    <div style="display: flex; flex-direction: row"  v-if="item['field'].type === 'string' || item['field'].type === 'text'">
+
+                        <el-input v-model="item['compare']"/>
+
+                    </div>
+
+
+                    <div style="display: flex; flex-direction: row"  v-if="item['field'].type === 'date' || item['field'].type === 'datetime' || getField(item['key'])?.type === 'time'">
+
+                        <el-date-picker v-model="item['compare']"
+                                        :type="item['field'].type"
+                                        :format="format(item['field'].type)"
+                        />
+
+                        <div v-if="item['op'] === 'between'" style="margin-left: 4px; margin-right: 4px">-</div>
+
+                        <el-date-picker v-if="item['op'] === 'between'"
+                                        v-model="item['compare_2']"
+                                        :type="item['field'].type"
+                                        :format="format(item['field'].type)"
+                        />
+                    </div>
+
+
+                    <div>
+
+<!--                        <el-select-->
+<!--                            ref="el"-->
+<!--                            v-else-if="field && (field.type === 'link')  && !field.isTree"-->
+<!--                            class="table-select"-->
+<!--                            :model-value="value"-->
+<!--                            :placeholder="$t('select')"-->
+<!--                            filterable-->
+<!--                            remote-->
+<!--                            clearable-->
+<!--                            remote-show-suffix-->
+<!--                            :remote-method="getLinkData"-->
+<!--                            :loading="isLoading"-->
+<!--                            @change="handleInput"-->
+<!--                        >-->
+<!--                            <el-option-->
+<!--                                v-for="item in linkData"-->
+<!--                                :key="item.id"-->
+<!--                                :label="item[displayProp]"-->
+<!--                                :value="item.id"-->
+<!--                            />-->
+<!--                        </el-select>-->
+
+                    </div>
+
 
                 </div>
-
 
             </el-form>
         </template>
@@ -54,7 +119,7 @@ import ItemList from "./ItemList.vue";
 import {ref} from "vue";
 import FieldSelect from "./FieldSelect.vue";
 import {useI18n} from "vue-i18n";
-import {FieldType} from "../model/field";
+import {FieldConfigInterface, FieldType} from "../model/field";
 const { t } = useI18n();
 
 interface Props {
@@ -70,7 +135,11 @@ interface Operator {
     types: FieldType[]
 }
 
-let _filters = ref<FilterItemInterface[]>([])
+interface FilterItem extends FilterItemInterface {
+    field?: FieldConfigInterface
+}
+
+let _filters = ref<FilterItem[]>([])
 
 let operators: Operator[] = [
     { key: '==',  title: t("filters.=="), types: ['number', 'string', 'text', 'link', 'list', 'date', 'time', 'datetime', 'bool', 'enum'] },
@@ -111,7 +180,7 @@ function getOperators(alias) {
     if (!alias || !props.filters.dataSource)
         return []
 
-    let field = props.filters.dataSource.getFieldByAlias(alias)
+    let field = getField(alias)
     if (!field) {
         console.warn(`Field by alias ${ alias } not found'`)
         return []
@@ -121,6 +190,21 @@ function getOperators(alias) {
     return operators.filter((operator) => {
         return operator.types.includes(field.type)
     })
+}
+
+function getField(alias) {
+    if (!alias || !props.filters || !props.filters.dataSource)
+        return undefined
+
+    return props.filters.dataSource.getFieldByAlias(alias)
+}
+
+function format(type) {
+    switch (type) {
+        case "date": return 'DD.MM.YYYY';
+        case "time": return 'HH:MM:SS';
+        case "datetime": return 'DD.MM.YYYY HH:MM:SS';
+    }
 }
 
 </script>
