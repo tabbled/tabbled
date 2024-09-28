66eeff86f2a435ec1460cd540a21232ac516a71c
wulff007 <wulff007@gmail.com>
2024-09-28 13:45:26 +0300

    fix sentry

--- public/env.template.js
+++ public/env.template.js
@@ -1,6 +1,6 @@
 (function() {
     window["env"] = window["env"] || {};
-    window["env"]["sentryDns"] = "${SENTRY_DNS}";
+    window["env"]["sentryDns"] = "${VITE_SENTRY_DNS}";
     window["env"]["appTitle"] = "${APP_TITLE}";
     window["env"]["appFavicon"] = "${APP_FAVICON}";
 })();
